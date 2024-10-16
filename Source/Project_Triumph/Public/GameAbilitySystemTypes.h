01dc178acd9d630a05719a2099185f55e7867143
Dark Motion Games <136380224+DarkMotionGames@users.noreply.github.com>
2024-10-16 13:59:36 +0530

    Custom Gameplay Effect Context Handle

---
+++ Source/Project_Triumph/Public/GameAbilitySystemTypes.h
@@ -0,0 +1,37 @@
+﻿// © 2024 DMG. All rights reserved.
+#pragma once
+
+#include "GameplayEffectTypes.h"
+#include "GameAbilitySystemTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FMainGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+
+public:
+
+	bool IsBlockedHit() const { return bIsBlockedHit; }
+	bool IsCriticalHit() const { return bIsCriticalHit; }
+
+	void SetIsBlockedHit(const bool InBlockHit) { bIsBlockedHit = InBlockHit; }
+	void SetIsCriticalHit(const bool InCriticalHit) { bIsCriticalHit = InCriticalHit; }
+	
+	/** Returns the actual struct used for serialization */
+	virtual UScriptStruct* GetScriptStruct() const override
+	{
+		return FGameplayEffectContext::StaticStruct();
+	}
+	
+	/** Custom serialization */
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess) override;
+
+protected:
+
+	UPROPERTY()
+	bool bIsBlockedHit = false;
+
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+	
+};
